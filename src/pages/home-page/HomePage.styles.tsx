f9ed19ee8c29b9ed2fe1088fb9d41a129afad6f1
Eduard <edmoiseenkov@gmail.com>
2022-06-04 17:34:52 +0300

    Added Polymorph & Lobster's section;

--- src/pages/home-page/HomePage.styles.tsx
+++ src/pages/home-page/HomePage.styles.tsx
@@ -1,4 +1,12 @@
-import { AccordionButtonProps, AccordionItemProps, AccordionProps, BoxProps } from '@chakra-ui/react';
+import {
+  AccordionButtonProps,
+  AccordionItemProps,
+  AccordionProps,
+  BoxProps,
+  HeadingProps,
+  TextProps
+} from '@chakra-ui/react';
+import { Gradient } from '@app/theme';
 
 export const Accordion: AccordionProps = {
   color: 'white',
@@ -26,4 +34,24 @@ export const AccordionButton: AccordionButtonProps = {
 export const Separator: BoxProps = {
   bg: 'linear-gradient(90deg, rgba(255, 251, 243, 0) 0%, rgba(255, 251, 243, 0.12) 11.98%, rgba(255, 251, 243, 0.12) 88.02%, rgba(255, 251, 243, 0) 100%)',
   h: '1px',
+};
+
+export const UniverseDropSubTitle: TextProps = {
+  bgGradient: Gradient.sea['100'],
+  bgClip: 'text',
+  fontSize: '12px',
+  fontWeight: 700,
+  letterSpacing: '4px',
+  mb: '24px',
+  textTransform: 'uppercase',
+};
+
+export const UniverseDropTitle: HeadingProps = {
+  fontSize: '64px',
+  mb: '24px',
+};
+
+export const UniverseDropText: TextProps = {
+  fontSize: '18px',
+  mb: '48px',
 };
--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,13 +1,18 @@
 import {
-  Accordion, AccordionButton, AccordionIcon,
-  AccordionItem, AccordionPanel,
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
   Box,
-  Button, Center,
+  Button,
+  Center,
   Container,
   Heading,
   HStack,
   Image,
-  Link, SimpleGrid,
+  Link,
+  SimpleGrid,
   Text,
   VStack
 } from '@chakra-ui/react';
@@ -29,6 +34,13 @@ import NFT4 from '@app/assets/images/nfts/4.png';
 import Group from '@app/assets/images/group.png';
 import LogoUniverse from '@app/assets/images/logo-universe.png';
 import LogoGraviton from '@app/assets/images/logo-graviton.png';
+import Polymorph from '@app/assets/images/polymorph.png';
+import PolymorphBlur1 from '@app/assets/images/polymorph-blur-1.png';
+import PolymorphBlur2 from '@app/assets/images/polymorph-blur-2.png';
+import Lobster from '@app/assets/images/lobster.png';
+import LobsterBlur1 from '@app/assets/images/lobster-blur-1.png';
+import LobsterBlur2 from '@app/assets/images/lobster-blur-2.png';
+import Border from '@app/assets/images/border.png';
 
 import CloudsFront from '@app/assets/images/clouds-front.png';
 import CloudsBack from '@app/assets/images/clouds-back.png';
@@ -457,9 +469,7 @@ export const HomePage: NextPage = () => {
             </Box>
           </HStack>
         </Container>
-        <Container sx={{
-          pb: '106px',
-        }}>
+        <Container>
           <Box sx={{
             background: 'rgba(255, 251, 243, 0.2)',
             boxShadow: '0px 8px 32px rgba(35, 19, 27, 0.16), inset 0px 0px 1px 1px rgba(255, 255, 255, 0.2)',
@@ -496,12 +506,154 @@ export const HomePage: NextPage = () => {
           </Box>
         </Container>
 
+        <Container py={'128px'}>
+          <HStack spacing={'48px'}>
+            <Box flex={1}>
+              <Box sx={{
+                bg: `url(${Border}) center / cover`,
+                boxSize: '400px',
+                padding: '32px',
+                pos: 'relative',
+                m: 'auto',
+                zIndex: 0,
+              }}>
+                <Image src={Polymorph} />
+                <Image src={PolymorphBlur1} sx={{
+                  boxSize: '160px',
+                  pos: 'absolute',
+                  left: '-24px',
+                  top: '-24px',
+                  zIndex: -1,
+                }} />
+                <Image src={PolymorphBlur2} sx={{
+                  boxSize: '160px',
+                  pos: 'absolute',
+                  right: '-24px',
+                  bottom: '-24px',
+                  zIndex: -1,
+                }} />
+                <Box sx={{
+                  bg: 'linear-gradient(135deg, #CD4097 0%, #5C81E4 100%)',
+                  filter: 'blur(50px)',
+                  boxSize: '148px',
+                  opacity: 0.3,
+                  pos: 'absolute',
+                  left: '-24px',
+                  bottom: '-24px',
+                  zIndex: -1,
+                }} />
+                <Box sx={{
+                  bg: 'linear-gradient(45deg, #F34078 0%, #FB884D 100%)',
+                  filter: 'blur(50px)',
+                  boxSize: '148px',
+                  opacity: 0.3,
+                  pos: 'absolute',
+                  right: '-24px',
+                  top: '-24px',
+                  zIndex: -1,
+                }} />
+              </Box>
+            </Box>
+            <Box flex={1} color={'white'}>
+              <Text {...s.UniverseDropSubTitle}>{`Polymorphs didn't see this coming!`}</Text>
+              <Heading {...s.UniverseDropTitle}>Polymorphs</Heading>
+              <Text {...s.UniverseDropText}>
+                ${`They got more than what they bargained for when they volunteered for the "bleisure" (business-leisure) trip to explore Metaversia. Only the very skilled and expert Polymorphs were chosen for this expedition as they tough it out on the most challenging mission they've ever been on. Many arrived in Metaversia, but only a few will get out.`}
+              </Text>
+              <HStack spacing={'16px'}>
+                <Button>View on Marketplace</Button>
+                <Button variant={'ghost'}>Learn More</Button>
+              </HStack>
+            </Box>
+          </HStack>
+        </Container>
+        <Container>
+          <Box {...s.Separator} />
+        </Container>
+        <Container py={'128px'}>
+          <HStack spacing={'48px'}>
+            <Box flex={1} color={'white'}>
+              <Text {...s.UniverseDropSubTitle}>{`Deities of an Underwater World`}</Text>
+              <Heading {...s.UniverseDropTitle}>Lobby Lobsters</Heading>
+              <Text {...s.UniverseDropText}>
+                ${`How did they create an Atlantean water world right underneath Metaversia without the humans' notice? What other mysterious secrets do the Lobby Lobsters hold? To the Deviants, these Lobsters of gods to be worshipped, but where did they come from if they didn't mutate because of the blast? Or were they here all along?`}
+              </Text>
+              <HStack spacing={'16px'}>
+                <Button>View on Marketplace</Button>
+                <Button variant={'ghost'}>Learn More</Button>
+              </HStack>
+            </Box>
+            <Box flex={1}>
+              <Box sx={{
+                bg: `url(${Border}) center / cover`,
+                boxSize: '400px',
+                padding: '32px',
+                pos: 'relative',
+                m: 'auto',
+                zIndex: 0,
+              }}>
+                <Image src={Lobster} />
+                <Image src={LobsterBlur1} sx={{
+                  boxSize: '160px',
+                  pos: 'absolute',
+                  right: '-24px',
+                  top: '-24px',
+                  zIndex: -1,
+                }} />
+                <Image src={LobsterBlur2} sx={{
+                  boxSize: '160px',
+                  pos: 'absolute',
+                  left: '-24px',
+                  bottom: '-24px',
+                  zIndex: -1,
+                }} />
+                <Box sx={{
+                  bg: 'linear-gradient(135deg, #CD4097 0%, #5C81E4 100%)',
+                  filter: 'blur(50px)',
+                  boxSize: '148px',
+                  opacity: 0.3,
+                  pos: 'absolute',
+                  right: '-24px',
+                  bottom: '-24px',
+                  zIndex: -1,
+                }} />
+                <Box sx={{
+                  bg: 'linear-gradient(45deg, #F34078 0%, #FB884D 100%)',
+                  filter: 'blur(50px)',
+                  boxSize: '148px',
+                  opacity: 0.3,
+                  pos: 'absolute',
+                  left: '-24px',
+                  top: '-24px',
+                  zIndex: -1,
+                }} />
+              </Box>
+            </Box>
+          </HStack>
+        </Container>
+
         <Box as={'footer'} sx={{
           bg: '#2D1A3F',
           color: 'cream.100',
         }}>
           <Container>
-            <Box padding={'88px 0 100px'} textAlign={'center'}>
+            <Box padding={'88px 0 100px'} textAlign={'center'} sx={{
+              pos: 'relative',
+              _before: {
+                bg: 'linear-gradient(45deg, #F34078 0%, #FB884D 100%)',
+                borderRadius: '50%',
+                filter: 'blur(70px)',
+                opacity: 0.6,
+                pos: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%)',
+                // filter: 'blur(160px)',
+                content: '""',
+                w: '648px',
+                h: '106px',
+              },
+            }}>
               <Heading fontSize={'40px'} mb={'16px'}>Our Partners</Heading>
               <Text fontSize={'18px'} mb={'56px'}>Metaversian Republic gets by with a little help from our friends:</Text>
               <SimpleGrid columns={partners.length} spacing={'48px'}>
@@ -511,6 +663,7 @@ export const HomePage: NextPage = () => {
                     boxShadow: '0px 8px 32px rgba(35, 19, 27, 0.16), inset 0px 0px 1px 1px rgba(255, 255, 255, 0.2)',
                     backdropFilter: 'blur(16px)',
                     borderRadius: '16px',
+                    cursor: 'pointer',
                     padding: '46px',
                   }}>
                     <Image src={p.logo} h={p.h} />
@@ -540,7 +693,7 @@ export const HomePage: NextPage = () => {
                 </SimpleGrid>
                 <HStack spacing={'16px'} flex={1} justifyContent={'flex-end'}>
                   {socials.map((icon, i) => (
-                    <Center sx={{
+                    <Center key={i} sx={{
                       bg: 'rgba(255, 251, 243, 0.1)',
                       borderRadius: 'full',
                       boxSize: '56px',
