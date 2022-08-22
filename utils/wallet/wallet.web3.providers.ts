6134d724603f3853388217fcd3f360246d1e3844
valeri.ivanchev@limechain.tech <valeri.ivanchev@limechain.tech>
2022-08-22 11:32:56 +0300

    new env

--- utils/wallet/wallet.web3.providers.ts
+++ utils/wallet/wallet.web3.providers.ts
@@ -2,7 +2,7 @@ import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
 export const walletConnectProvider = new WalletConnectProvider({
-  infuraId: "1745e014e2ed4047acdaa135e869a11b"
+  infuraId: <string>process.env.INFURAID
 });
 
 export const getCoinBaseProvider = () => {
@@ -12,7 +12,7 @@ export const getCoinBaseProvider = () => {
     darkMode: false
   });
 
-  const coinbaseWalletProvider = coinbaseWallet.makeWeb3Provider('https://mainnet.infura.io/v3/1745e014e2ed4047acdaa135e869a11b', 1);
+  const coinbaseWalletProvider = coinbaseWallet.makeWeb3Provider(`https://mainnet.infura.io/v3/${process.env.INFURAID}`, 1);
 
   return coinbaseWalletProvider;
 }
