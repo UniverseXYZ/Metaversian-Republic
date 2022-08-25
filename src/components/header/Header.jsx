3720b891aff6eff1225de4400731c4fa3e42ca87
gope-gope <georgigeorgiew97@gmail.com>
2022-08-25 14:05:19 +0300

    Added profile dropdown

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,14 @@
-import Logo from '@app/assets/images/Logo.svg';
-import {
-  Box,
-  Button,
-  HStack,
-  Image,
-  Link,
-  Text
-} from '@chakra-ui/react';
+import Logo from "@app/assets/images/Logo.svg";
+import { Box, Button, HStack, Image, Link, Text } from "@chakra-ui/react";
 import PropTypes from "prop-types";
-import Popup from 'reactjs-popup';
+import Popup from "reactjs-popup";
+import { shortenEthereumAddress } from "../../../utils/helpers/format.js";
+import arrowUp from "../../assets/icons/arrow-up.svg";
+import ethIcon from "../../assets/icons/eth-icon.svg";
+import walletIcon from "../../assets/icons/wallet-icon.svg";
+import profileIcon from "../../assets/images/profile-tab-icon.png";
 import SelectWalletPopup from "../popups/SelectWalletPopup";
+import classes from "./Header.module.scss";
 
 const Header = (props) => {
   const {
@@ -19,50 +18,92 @@ const Header = (props) => {
     selectedWallet,
     setSelectedWallet,
     walletAddress,
-    header
+    header,
   } = props;
 
   return (
-    <Box as={'header'} sx={{
-      alignItems: 'center',
-      bg: 'rgba(28, 5, 49, 0.1)',
-      backdropFilter: 'blur(16px)',
-      display: 'flex',
-      justifyContent: 'space-between',
-      padding: '23px 24px',
-      zIndex: 1,
-    }}>
-      <Image src={Logo} alt={'Logo'} h={'32px'} w={'164px'} />
-      <HStack spacing={'40px'}>
+    <Box
+      as={"header"}
+      sx={{
+        alignItems: "center",
+        bg: "rgba(28, 5, 49, 0.1)",
+        backdropFilter: "blur(16px)",
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "23px 24px",
+        zIndex: 1,
+        position: "relative",
+      }}
+    >
+      <Image src={Logo} alt={"Logo"} h={"32px"} w={"164px"} />
+      <HStack spacing={"40px"}>
         {header.map((section, i) => (
-          <Link key={i} href={"#"} sx={{
-            color: 'white',
-          }}>{section.name}</Link>
+          <Link
+            key={i}
+            href={"#"}
+            sx={{
+              color: "white",
+            }}
+          >
+            {section.name}
+          </Link>
         ))}
       </HStack>
       <Box>
-        {!walletAddress ? <Popup
-          closeOnDocumentClick={false}
-          trigger={
-            <Button variant={'ghost'}>Connect Wallet</Button>
-          }
-        >
-          {(close) => (
-            <SelectWalletPopup
-              close={close}
-              handleConnectWallet={handleConnectWallet}
-              showInstallWalletPopup={showInstallWalletPopup}
-              setShowInstallWalletPopup={setShowInstallWalletPopup}
-              selectedWallet={selectedWallet}
-              setSelectedWallet={setSelectedWallet}
-            />
-          )}
-
-        </Popup> :
-          <Text>
-            {walletAddress}
+        {!walletAddress ? (
+          <Popup
+            closeOnDocumentClick={false}
+            trigger={<Button variant={"ghost"}>Connect Wallet</Button>}
+          >
+            {(close) => (
+              <SelectWalletPopup
+                close={close}
+                handleConnectWallet={handleConnectWallet}
+                showInstallWalletPopup={showInstallWalletPopup}
+                setShowInstallWalletPopup={setShowInstallWalletPopup}
+                selectedWallet={selectedWallet}
+                setSelectedWallet={setSelectedWallet}
+              />
+            )}
+          </Popup>
+        ) : (
+          <Text className={classes["profile-button-wrapper"]}>
+            <Button className={classes["profile-button"]} variant={"ghost"}>
+              <>
+                <Image src={profileIcon} width="24px" height="24px" alt="" />
+                {shortenEthereumAddress(walletAddress)}
+                <Image className={classes["arrow"]} src={arrowUp} />
+              </>
+            </Button>
+            <div className={classes["dropdown-profile"]}>
+              <div className={classes["header"]}>
+                <div className={classes["eth-address"]}>
+                  <Image src={walletIcon} />
+                  <span>
+                    {walletAddress ? shortenEthereumAddress(walletAddress) : ""}
+                  </span>
+                </div>
+                <div className={classes["balance"]}>
+                  <Image src={ethIcon} />
+                  <span>Balance ETH</span>
+                </div>
+                <div type="button">
+                  <div>Polymorphs: {5}</div>
+                </div>
+              </div>
+              <div className={classes["body"]}>
+                <Button variant={"ghost"}>
+                  {() => {
+                    setIsAccountDropdownOpened(false);
+                    signOut();
+                    router.push("/");
+                  }}
+                  <div>Disconnect</div>
+                </Button>
+              </div>
+            </div>
           </Text>
-        }
+        )}
       </Box>
     </Box>
   );
@@ -75,7 +116,7 @@ Header.propTypes = {
   selectedWallet: PropTypes.string.isRequired,
   setSelectedWallet: PropTypes.func.isRequired,
   walletAddress: PropTypes.string.isRequired,
-  header: PropTypes.array.isRequired
+  header: PropTypes.array.isRequired,
 };
 
 export default Header;
