628236980a271a9ebcf9f899a51636b2dfdad58b
gope-gope <georgigeorgiew97@gmail.com>
2022-08-25 18:19:22 +0300

    Added game section

--- src/components/nft-sections/PolymorphsSection.jsx
+++ src/components/nft-sections/PolymorphsSection.jsx
@@ -1,80 +1,104 @@
-import Border from '@app/assets/images/border.png';
-import PolymorphBlur1 from '@app/assets/images/polymorph-blur-1.png';
-import PolymorphBlur2 from '@app/assets/images/polymorph-blur-2.png';
-import Polymorph from '@app/assets/images/polymorph.png';
+import Border from "@app/assets/images/border.png";
+import PolymorphBlur1 from "@app/assets/images/polymorph-blur-1.png";
+import PolymorphBlur2 from "@app/assets/images/polymorph-blur-2.png";
+import Polymorph from "@app/assets/images/polymorph.png";
 import {
   Box,
-  Button, Container, Heading,
+  Button,
+  Container,
+  Heading,
   HStack,
   Image,
-  Text
-} from '@chakra-ui/react';
-import * as s from '../../pages/home-page/HomePage.styles';
+  Text,
+} from "@chakra-ui/react";
+import * as s from "../../pages/home-page/HomePage.styles";
 
 const PolymorphsSection = () => {
   return (
-    <Container py={'128px'} id="polymorphsSection">
-      <HStack spacing={'48px'}>
+    <Container py={"128px"} id="polymorphsSection">
+      <HStack spacing={"48px"}>
         <Box flex={1}>
-          <Box sx={{
-            bg: `url(${Border}) center / cover`,
-            boxSize: '400px',
-            padding: '32px',
-            pos: 'relative',
-            m: 'auto',
-            zIndex: 0,
-          }}>
+          <Box
+            sx={{
+              bg: `url(${Border}) center / cover`,
+              boxSize: "400px",
+              padding: "32px",
+              pos: "relative",
+              m: "auto",
+              zIndex: 0,
+            }}
+          >
             <Image src={Polymorph} />
-            <Image src={PolymorphBlur1} sx={{
-              boxSize: '160px',
-              pos: 'absolute',
-              left: '-24px',
-              top: '-24px',
-              zIndex: -1,
-            }} />
-            <Image src={PolymorphBlur2} sx={{
-              boxSize: '160px',
-              pos: 'absolute',
-              right: '-24px',
-              bottom: '-24px',
-              zIndex: -1,
-            }} />
-            <Box sx={{
-              bg: 'linear-gradient(135deg, #CD4097 0%, #5C81E4 100%)',
-              filter: 'blur(50px)',
-              boxSize: '148px',
-              opacity: 0.3,
-              pos: 'absolute',
-              left: '-24px',
-              bottom: '-24px',
-              zIndex: -1,
-            }} />
-            <Box sx={{
-              bg: 'linear-gradient(45deg, #F34078 0%, #FB884D 100%)',
-              filter: 'blur(50px)',
-              boxSize: '148px',
-              opacity: 0.3,
-              pos: 'absolute',
-              right: '-24px',
-              top: '-24px',
-              zIndex: -1,
-            }} />
+            <Image
+              src={PolymorphBlur1}
+              sx={{
+                boxSize: "160px",
+                pos: "absolute",
+                left: "-24px",
+                top: "-24px",
+                zIndex: -1,
+              }}
+            />
+            <Image
+              src={PolymorphBlur2}
+              sx={{
+                boxSize: "160px",
+                pos: "absolute",
+                right: "-24px",
+                bottom: "-24px",
+                zIndex: -1,
+              }}
+            />
+            <Box
+              sx={{
+                bg: "linear-gradient(135deg, #CD4097 0%, #5C81E4 100%)",
+                filter: "blur(50px)",
+                boxSize: "148px",
+                opacity: 0.3,
+                pos: "absolute",
+                left: "-24px",
+                bottom: "-24px",
+                zIndex: -1,
+              }}
+            />
+            <Box
+              sx={{
+                bg: "linear-gradient(45deg, #F34078 0%, #FB884D 100%)",
+                filter: "blur(50px)",
+                boxSize: "148px",
+                opacity: 0.3,
+                pos: "absolute",
+                right: "-24px",
+                top: "-24px",
+                zIndex: -1,
+              }}
+            />
           </Box>
         </Box>
-        <Box flex={1} color={'white'}>
-          <Text {...s.UniverseDropSubTitle}>{`Polymorphs didn't see this coming!`}</Text>
+        <Box flex={1} color={"white"}>
+          <Text
+            {...s.UniverseDropSubTitle}
+          >{`Polymorphs didn't see this coming!`}</Text>
           <Heading {...s.UniverseDropTitle}>Polymorphs</Heading>
           <Text {...s.UniverseDropText}>
-            ${`They got more than what they bargained for when they volunteered for the "bleisure" (business-leisure) trip to explore Metaversia. Only the very skilled and expert Polymorphs were chosen for this expedition as they tough it out on the most challenging mission they've ever been on. Many arrived in Metaversia, but only a few will get out.`}
+            $
+            {`They got more than what they bargained for when they volunteered for the "bleisure" (business-leisure) trip to explore Metaversia. Only the very skilled and expert Polymorphs were chosen for this expedition as they tough it out on the most challenging mission they've ever been on. Many arrived in Metaversia, but only a few will get out.`}
           </Text>
-          <HStack spacing={'16px'}>
+          <HStack spacing={"16px"}>
             <Button>View on Marketplace</Button>
-            <Button variant={'ghost'} onClick={() => { window.open('https://polymorphs.universe.xyz/') }}>Learn More</Button>
+            <Button
+              variant={"ghost"}
+              onClick={() => {
+                window.open("https://polymorphs.universe.xyz/");
+              }}
+            >
+              Learn More
+            </Button>
           </HStack>
         </Box>
       </HStack>
     </Container>
-  )
-}
+  );
+};
 
 export default PolymorphsSection;
---
+++ src/components/zombie-game/GameSection.jsx
@@ -0,0 +1,92 @@
+import { Button } from "@chakra-ui/react";
+import Image from "next/image";
+import Popup from "reactjs-popup";
+import BlurElipse from "../../assets/images/blur-elipse-1.png";
+import iOSStore from "../../assets/images/iOS-store.png";
+import zombieHand from "../../assets/images/zombie-hand.png";
+import SelectWalletPopup from "../popups/SelectWalletPopup";
+import classes from "./GameSection.module.scss";
+
+const GameSection = ({
+  handleConnectWallet,
+  showInstallWalletPopup,
+  setShowInstallWalletPopup,
+  selectedWallet,
+  setSelectedWallet,
+  walletAddress,
+}) => {
+  return (
+    <div className={classes["container"]}>
+      <div className={classes["subcontainer-info"]}>
+        <div className={classes["heading"]}>
+          Who Inhabits the Metaversian Republic?
+        </div>
+        <div className={classes["body"]}>
+          Metaversia was previously a paradise resort island inhabited by
+          humans. After a fatal nuclear blast, all Metaversian humans
+          disappeared, leaving behind the Deviants, Polymorphs and their new
+          friends the Lobby Lobsters.
+        </div>
+        <div className={classes["elipse"]}>
+          <Image src={BlurElipse} width={1052} height={500} />
+        </div>
+      </div>
+      <div className={classes["subcontainer-wrapper"]}>
+        <div className={classes["subcontainer-game"]}>
+          <div className={classes["info"]}>
+            <div className={classes["heading"]}>Mobile Game</div>
+            <div className={classes["subheading"]}>
+              Kill zombies with your Polymorph, and challenge with your friends.
+            </div>
+            <div className={classes["steps"]}>
+              <span>1</span>
+              <div>Connect your wallet and generate a code</div>
+            </div>
+            <div className={classes["steps"]}>
+              <span>2</span>
+              <div>
+                Enter code in the mobile game, choose a Polymorph, and play
+              </div>
+            </div>
+            <div className={classes["ios-button"]}>
+              <button
+                onClick={() => window.open("https://www.apple.com/app-store/")}
+              >
+                <img src={iOSStore} />
+              </button>
+            </div>
+          </div>
+          <div className={classes["zombie-hand"]}>
+            <Image
+              src={zombieHand}
+              width={988}
+              height={577}
+              layout="responsive"
+            />
+          </div>
+          <div>
+            {!walletAddress && (
+              <Popup
+                closeOnDocumentClick={false}
+                trigger={<Button>Connect Wallet</Button>}
+              >
+                {(close) => (
+                  <SelectWalletPopup
+                    close={close}
+                    handleConnectWallet={handleConnectWallet}
+                    showInstallWalletPopup={showInstallWalletPopup}
+                    setShowInstallWalletPopup={setShowInstallWalletPopup}
+                    selectedWallet={selectedWallet}
+                    setSelectedWallet={setSelectedWallet}
+                  />
+                )}
+              </Popup>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GameSection;
