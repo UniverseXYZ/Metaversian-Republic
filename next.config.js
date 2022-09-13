b7e49abe5e9d997cac20b49a8b6501950c6ffd4e
Stanislav Trenev <stanislav.trenev@limechain.tech>
2022-09-13 04:38:18 +0300

    Update Next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const withImages = require('next-images');
-const withPlugins = require('next-compose-plugins');
-const withVideos = require('next-videos')
+const withImages = require("next-images");
+const withPlugins = require("next-compose-plugins");
+const withVideos = require("next-videos");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -8,43 +8,45 @@ const nextConfig = {
   images: {
     disableStaticImages: true,
   },
-}
+};
 
 module.exports = withPlugins(
   [
     withVideos,
-    [withImages, {
-      fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'ico', 'webp', 'jp2', 'avif']
-    }],
+    [
+      withImages,
+      {
+        fileExtensions: ["jpg", "jpeg", "png", "gif", "ico", "webp", "jp2", "avif"],
+      },
+    ],
   ],
   {
     trailingSlash: true,
     reactStrictMode: false,
     images: {
       disableStaticImages: true,
-      domains: [
-        'ipfs.io',
-        'storage.googleapis.com',
-      ],
+      domains: ["ipfs.io", "storage.googleapis.com"],
     },
     webpack(config, options) {
       config.module.rules.push({
         test: /\.mp3$/,
         use: {
-          loader: 'file-loader',
+          loader: "file-loader",
         },
       });
 
       config.module.rules.push({
         test: /\.svg$/,
-        use: ['@svgr/webpack', 'url-loader'],
+        use: ["@svgr/webpack", "url-loader"],
       });
       return config;
     },
     env: {
       INFURAID: process.env.INFURAID,
       THE_GRAPH_V1_URL: process.env.THE_GRAPH_V1_URL,
-      THE_GRAPH_V2_URL: process.env.THE_GRAPH_V2_URL
-    }
+      THE_GRAPH_V2_URL: process.env.THE_GRAPH_V2_URL,
+      ZOMBIE_GAME_CLOUDFUNCTION_INVOKER: process.env.ZOMBIE_GAME_CLOUDFUNCTION_INVOKER,
+      GET_ID_URL: process.env.GET_ID_URL,
+    },
   }
 );
