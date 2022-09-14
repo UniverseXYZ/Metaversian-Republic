547bfc209ceaa80600e487428503aa2ed3b07faa
nadezhda <nadezhda@devlabs.bg>
2022-09-14 19:06:23 +0300

    Added: deviants component to hero

--- src/components/deviants-mint/DeviantsMintsComponent.jsx
+++ src/components/deviants-mint/DeviantsMintsComponent.jsx
@@ -12,7 +12,7 @@ import { useMemo, useState } from 'react';
 const MINT_PRICE = 0.0666;
 const MAX_MINT_AMOUNT = 10000;
 
-const DeviantsMinComponent = () => {
+const DeviantsMinComponent = ({ isShort }) => {
   const [mintAmount, setMintAmount] = useState(1);
   const [minted, setMinted] = useState(6320);
   const mintTotalPrice = useMemo(() => {
@@ -36,24 +36,24 @@ const DeviantsMinComponent = () => {
         }}>
           Mint a Deviant
         </Heading>
-        <Flex gap={{ base: 8, md: 0 }} w={'100%'} flexDir={{ base: "column-reverse", md: "column" }}  >
+        <Flex gap={{ base: 8, md: isShort ? 8 : 0 }} w={'100%'} flexDir={{ base: "column-reverse", md: isShort ? "column-reverse" : "column" }}  >
           <Progress value={minted} max={MAX_MINT_AMOUNT} h={'48px'} mb={'38px'}>
             <Text color={'white'} fontSize={'16px'} fontWeight={'bold'} textAlign={'center'}>
               {minted}/{MAX_MINT_AMOUNT} already minted
             </Text>
           </Progress>
 
-          <Flex gap={8}  flexDir={{ base: "column", md: "row" }}  justifyContent={'space-between'} alignItems={'center'}>
+          <Flex gap={8}  flexDir={{ base: "column", md: isShort ? "column" : "row" }}  justifyContent={'space-between'} alignItems={'center'}>
             <AmountSelector
               color={'cream.100'}
               minW={'168px'}
-              w={{ base: '100%', md: 'auto' }}
+              w={{ base: '100%', md: isShort ? '100%' : 'auto' }}
               size={'lg'}
               min={1}
               value={mintAmount}
               onChange={(value) => setMintAmount(value)}
             />
-            <Button size={'lg'} w={{ base: '100%', md: 'auto' }}>Mint for {mintTotalPrice} ETH</Button>
+            <Button size={'lg'} w={{ base: '100%', md: isShort ? '100%' : 'auto' }}>Mint for {mintTotalPrice} ETH</Button>
             <Text color={'cream.40'}>Minting ends in 2d, 20h, 34m</Text>
           </Flex>
         </Flex>
