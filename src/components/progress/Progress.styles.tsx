21f9267ff5f956c6d011396567e802042b46f023
nadezhda <nadezhda@devlabs.bg>
2022-09-19 17:46:24 +0300

    Fix percentage slider

--- src/components/progress/Progress.styles.tsx
+++ src/components/progress/Progress.styles.tsx
@@ -1,33 +1,32 @@
 import { BoxProps } from '@chakra-ui/react';
 
 export const Wrapper: BoxProps = {
+  display: 'flex',
   alignItems: 'center',
   background: 'rgba(28, 5, 49, 0.16)',
-  borderRadius: '100px',
   boxShadow: 'inset 2px 2px 4px rgba(35, 19, 27, 0.05)',
-  display: 'flex',
-  flexDir: 'row',
+  width: "100%",
+  height: "36px",
   padding: '4px',
-  height: '36px',
+  overflow: "hidden",
+  borderRadius: "100px",
+  position: 'relative'
 };
 
 export const Progress: BoxProps = {
+  display: 'flex',
   alignItems: 'center',
+  justifyContent: 'center',
+  width: "100%",
+  height: "100%",
   background: 'linear-gradient(45deg, #6C42E3 0%, #01AEE9 100%)',
   border: '1px solid rgba(255, 251, 243, 0.1)',
   boxShadow: '0px 0px 16px rgba(35, 19, 27, 0.4), inset 0px 0px 8px rgba(255, 255, 255, 0.4)',
-  borderRadius: '100px 0 0 100px',
-  display: 'flex',
-  h: '100%',
-  justifyContent: 'center',
-
-  sx: {
-    backgroundOrigin: 'border-box',
-  }
 };
 
 export const Label: BoxProps = {
   left: '50%',
   position: 'absolute',
   transform: 'translateX(-50%)',
+  width: '100%'
 };
