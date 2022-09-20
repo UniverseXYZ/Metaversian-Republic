226e3cf0c612fa43f438b416d24c129510133531
valeri.ivanchev@limechain.tech <valeri.ivanchev@limechain.tech>
2022-09-20 16:13:34 +0300

    Add the popup logic in the component

--- src/components/deviants-mint/DeviantsMintsComponent.jsx
+++ src/components/deviants-mint/DeviantsMintsComponent.jsx
@@ -11,10 +11,12 @@ import { parseEther } from 'ethers/lib/utils';
 import { useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Popup from 'reactjs-popup';
+import { setShowMintError, setShowProcessingPopup, setShowSuccessfulPopup } from 'utils/app/app.slice';
+import { useAppDispatch } from 'utils/dispatch';
 import { abi } from 'utils/wallet/deviants.abi.json';
 import { getMintedDeviants } from 'utils/wallet/deviants.helpers';
 import useWallet from 'utils/wallet/useWallet';
-import { selectDiscountDeviantCount, selectWallet, selectWalletAddress } from 'utils/wallet/wallet.slice';
+import { selectBalance, selectDiscountDeviantCount, selectWallet, selectWalletAddress } from 'utils/wallet/wallet.slice';
 import SelectWalletPopup from '../popups/SelectWalletPopup';
 
 const MINT_PRICE = 0.0666;
@@ -24,6 +26,7 @@ const DeviantsMinComponent = ({ isShort }) => {
   const [mintAmount, setMintAmount] = useState(1);
   const walletAddress = useSelector(selectWalletAddress);
   const [minted, setMinted] = useState(0);
+  const balance = useSelector(selectBalance);
   const { callContract } = useWallet();
   const [showInstallWalletPopup, setShowInstallWalletPopup] = useState(false);
   const [selectedWallet, setSelectedWallet] = useState('');
@@ -33,6 +36,7 @@ const DeviantsMinComponent = ({ isShort }) => {
   const mintTotalPrice = useMemo(() => {
     return +(mintAmount * MINT_PRICE).toFixed(4);
   }, [mintAmount]);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (minted == 0) {
@@ -44,11 +48,43 @@ const DeviantsMinComponent = ({ isShort }) => {
   }, [minted]);
 
   const handleMint = async () => {
-    callContract(abi, "mint", mintAmount, { value: parseEther((mintAmount * MINT_PRICE).toString()) });
+    if (+balance < (mintAmount * MINT_PRICE)) {
+      dispatch(setShowMintError(true));
+      return;
+    }
+    const mintTnx = await callContract(abi, "mint", mintAmount, { value: parseEther((mintAmount * MINT_PRICE).toString()) });
+    dispatch(setShowProcessingPopup(true));
+
+    mintTnx
+      .wait()
+      .then((txReceipt) => {
+        if (txReceipt.status != 1) {
+          console.log("Mint fail");
+          return;
+        }
+        dispatch(setShowProcessingPopup(false));
+        dispatch(setShowSuccessfulPopup(true));
+      })
   }
 
   const handleDiscountMint = async () => {
-    callContract(abi, "discountMint", mintAmount, { value: parseEther((mintAmount * (MINT_PRICE / 2)).toString()) });
+    if (+balance < (mintAmount * MINT_PRICE)) {
+      dispatch(setShowMintError(true));
+      return;
+    }
+    const mintTnx = await callContract(abi, "discountMint", mintAmount, { value: parseEther((mintAmount * (MINT_PRICE / 2)).toString()) });
+    dispatch(setShowProcessingPopup(true));
+
+    mintTnx
+      .wait()
+      .then((txReceipt) => {
+        if (txReceipt.status != 1) {
+          console.log("Mint fail");
+          return;
+        }
+        dispatch(setShowProcessingPopup(false));
+        dispatch(setShowSuccessfulPopup(true));
+      })
   }
 
   const discountMintVisability = useMemo(() => {
